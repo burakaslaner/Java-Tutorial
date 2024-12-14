623f0da13c4907ea7ada64bc46e8df287cfebb94
Burak Aslaner <burakaslaner_17@hotmail.com>
2024-12-14 11:40:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Birinci sayıyı giriniz: ");
+        int sayi1 = input.nextInt();
+        System.out.println("İkinci sayıyı giriniz: ");
+        int sayi2 = input.nextInt();
+
+        for(int i = sayi1; i <= sayi2; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
